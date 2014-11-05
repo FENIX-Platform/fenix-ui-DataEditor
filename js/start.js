117aae147958951c95385be3de8895a9e27fbaa9
DanieleO <daniele.olivotti@fao.org>
2014-11-05 18:40:42 +0100

    Debug

--- js/start.js
+++ js/start.js
@@ -18,23 +18,24 @@ define([
             return "http://faostat3.fao.org:7799/v2/msd/resources/" + system + "/" + version;
         } });
 
-        $('#btnEN').click(function () {
+        /*$('#btnEN').click(function () {
             setLang('en');
         });
         $('#btnFR').click(function () {
             setLang('fr');
-        });
+        });*/
         dataEditWr.setDataLang(localStorage.getItem('locale'));
 
-        /*Test*/
+        //Test
         //var metaAdapter = { source: { url: 'http://localhost:1031/dataUpload_03/js/z_tmp/dataset_233CPD010.txt'} };
-        var metaAdapter = { source: { url: 'http://faostat3.fao.org:7799/v2/msd/resources/metadata/dan2/1.0?dsd=true'} };
+        /*var metaAdapter = { source: { url: 'http://faostat3.fao.org:7799/v2/msd/resources/metadata/dan2/1.0?dsd=true'} };
         dataEditWr.setMetaAdapter(metaAdapter);
         dataEditWr.setData([
             ["01059", 2000, 4],
-            [null, 2001, null]]);
+            [null, 2001, null]]);*/
         //dataEditWr.setColsAndData(JSON.parse(testDatasetMETA).dsd.columns, JSON.parse(testDatasetDATA));
-        /*END Test*/
+        //END Test
+
 
 
     }
