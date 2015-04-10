a5f067eab357c8c8eeaf6d7eccf87926bc33e3a6
danielefenix <daniele.salvatore@fao.org>
2015-04-10 17:11:52 +0200

    Default external URL

--- js/DataEditor/dataConnectors/Connector_D3S.js
+++ js/DataEditor/dataConnectors/Connector_D3S.js
@@ -28,9 +28,9 @@ define([
             getDataUrl: "resources/data",
             getMetaAndDataUrl: "resources",
             //Codelists
-            codelistUrl: "http://faostat3.fao.org:7799/v2/msd/resources",
-            codelistMetaUrl: "http://faostat3.fao.org:7799/v2/msd/resources/metadata",
-            codelistFilteredUrl: "http://faostat3.fao.org:7799/v2/msd/codes/filter",
+            codelistUrl: "http://fenix.fao.org/d3s_dev/msd/resources/data",
+            codelistMetaUrl: "http://fenix.fao.org/d3s_dev/msd/resources/metadata",
+            codelistFilteredUrl: "http://fenix.fao.org/d3s_dev/msd/codes/filter",
 
             contextSystem: "CountrySTAT",
             datasource: "D3S"
