5d65b191a79a1819bab56f92e9993b8831190cc5
DanieleO-fenix <daniele.olivotti@fao.org>
2016-01-20 12:25:36 +0100

    Save and Upload CSV buttons are disabled while loading the resources

--- js/dataManagementCommons/FileUploadHelper.js
+++ js/dataManagementCommons/FileUploadHelper.js
@@ -1,9 +1,6 @@
 ﻿define(['jquery',
     'amplify'
-    //'text!fx-DataUpload/templates/DataUpload/FileUpload.htm',
-    //'i18n!fx-DataUpload/multiLang/DataUpload/nls/ML_DataUpload'
 ],
-//function ($, FileUploadHTML, mlRes) {
 function ($) {
     var widgetName = 'FileUploadHelper';
     var evtTextFileUploaded = 'textFileUploaded.' + widgetName + '.fenix';
@@ -62,78 +59,18 @@ function ($) {
         });
     }
 
+    FileUploadHelper.prototype.enabled = function (isEnabled) {
+        if (isEnabled) {
+            this.$uploadInput.removeAttr('disabled');
+        }
+        else {
+            this.$uploadInput.attr('disabled', 'disabled');
+        }
+    }
+
     FileUploadHelper.prototype.destroy = function () {
         this.$uploadInput.off('change');
     }
 
     return FileUploadHelper;
 });
-
-
-
-/*define(['jquery',
-    'text!fx-DataUpload/templates/DataUpload/FileUpload.htm',
-    'i18n!fx-DataUpload/multiLang/DataUpload/nls/ML_DataUpload'
-],
-function ($, FileUploadHTML, mlRes) {
-    var widgetName = 'TextFileUpload';
-    var evtTextFileUploaded = 'textFileUploaded.' + widgetName + '.fenix';
-    var defConfig = {
-        accept: ['csv'],
-        maxFileBytes: 0
-    };
-
-    function TextFileUpload(config) {
-        this.config = {};
-        $.extend(true, this.config, defConfig, config);
-        for (var i = 0; i < this.config.accept.length; i++)
-            this.config.accept[i] = this.config.accept[i].toLowerCase();
-        this.$container;
-        this.$uploadInput;
-    };
-
-    TextFileUpload.prototype.render = function (container) {
-        this.$container = container;
-        this.$container.html(FileUploadHTML);
-        this.initUploadInput();
-    }
-
-    TextFileUpload.prototype.reset = function () {
-        this.$uploadInput.val('');
-    }
-
-    TextFileUpload.prototype.initUploadInput = function () {
-        this.$uploadInput = this.$container.find('#fName');
-        var me = this;
-
-        this.$uploadInput.on('change', function (e) {
-            var ext = me.$uploadInput.val().split(".").pop().toLowerCase();
-            if ($.inArray(ext, me.config.accept) == -1) {
-                alert(mlRes.wrongFileType);
-                return false;
-            }
-            if (me.config.maxFileBytes != 0) {
-                if (e.target.files.item(0).size > me.config.maxFileBytes) {
-                    alert(mlRes.maxFileSizeIs.replace("%max%", (me.config.maxFileBytes / 1048576)));
-                    return false;
-                }
-            }
-            if (e.target.files != undefined) {
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    var str = e.target.result;
-                    me.$container.trigger(evtTextFileUploaded, str);
-                };
-                reader.readAsText(e.target.files.item(0));
-            }
-            return false;
-        });
-    }
-
-    TextFileUpload.prototype.destroy = function () {
-        this.$uploadInput.off('change');
-    }
-
-    return TextFileUpload;
-});
-*/
